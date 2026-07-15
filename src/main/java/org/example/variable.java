e421d87009654cc99783c57cf00016aa54484338
yogeshkag12 <yogikag@gmail.com>
2026-07-15 23:22:50 +0530

    added variable.java

---
+++ src/main/java/org/example/variable.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.net.StandardSocketOptions;
+import java.sql.SQLOutput;
+
+public class variable {
+    public static void main() {
+        int a = 10;
+        System.out.println(a);
+
+        String b = "java";
+        System.out.println(b);
+
+        Boolean c = true;
+        System.out.println(c);
+
+        char d = 'a';
+        System.out.println(d);
+
+        float e = 1.22f;
+        System.out.println(e);
+
+        double f = 1.344;
+        System.out.println(f);
+
+        byte g = -89;
+        System.out.println(g);
+
+        short h = -3380;
+        System.out.println(h);
+
+        long i = -888888888;
+        System.out.println(i);
+
+
+
+
+    }
+}
