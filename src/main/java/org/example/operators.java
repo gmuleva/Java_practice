73c085bf9bbde409fad474b0eb9f42ef17aac80d
yogeshkag12 <yogikag@gmail.com>
2026-07-16 01:14:16 +0530

    operators

---
+++ src/main/java/org/example/operators.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class operators {
+    public static void main() {
+        int a =10 ;
+        int b = 2;
+        System.out.println(a*b);
+        System.out.println(a / b);
+        System.out.println(a-b);
+        System.out.println(a+b);
+        System.out.println(a % b);
+
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(++a);
+
+        System.out.println(a>b && b>a);
+        System.out.println(a>b || b>a);
+
+        a +=5;
+        System.out.println(a);
+
+        a-=8;
+        System.out.println(a);
+
+        a *=2;
+        System.out.println(a);
+
+        a /=2;
+        System.out.println(a);
+
+
+
+    }
+}
